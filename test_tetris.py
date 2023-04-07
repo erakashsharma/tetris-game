d3dbf1bae1e74b37babd1d342aa2b1d1f614fc2d
erakashsharma <er.akashsharma@hotmail.com>
2023-04-07 17:57:11 +0530

    test cases added for new block, move left and move right

---
+++ test_tetris.py
@@ -0,0 +1,46 @@
+import unittest
+from tetris import Tetris
+
+
+class TestTetris(unittest.TestCase):
+
+    def setUp(self):
+        self.tetris = Tetris()
+
+    def test_new_block(self):
+        self.tetris.new_block()
+        self.assertIsNotNone(self.tetris.block)
+        self.assertIsNotNone(self.tetris.block_x)
+        self.assertIsNotNone(self.tetris.block_y)
+
+    def test_move_block_left(self):
+        # test move block left without collision
+        self.tetris.new_block()
+        initial_x = self.tetris.block_x
+        self.tetris.move_block_left()
+        self.assertEqual(self.tetris.block_x, initial_x-1)
+
+        # test move block left with collision
+        self.tetris.block_x = 0
+        initial_x = self.tetris.block_x
+        self.tetris.move_block_left()
+        self.assertEqual(self.tetris.block_x, initial_x)
+        self.assertIsNotNone(self.tetris.block)
+
+    def test_move_block_right(self):
+        # test move block right without collision
+        self.tetris.new_block()
+        initial_x = self.tetris.block_x
+        self.tetris.move_block_right()
+        self.assertEqual(self.tetris.block_x, initial_x+1)
+
+        # test move block right with collision
+        self.tetris.block_x = self.tetris.width - len(self.tetris.block[0])
+        initial_x = self.tetris.block_x
+        self.tetris.move_block_right()
+        self.assertEqual(self.tetris.block_x, initial_x)
+        self.assertIsNotNone(self.tetris.block)
+
+
+if __name__ == '__main__':
+    unittest.main()
